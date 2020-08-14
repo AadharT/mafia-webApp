c5769962f1e9a17dc626ed9092e9f971fc0ca4cd
Aadhar <tyagi.aadhar@gmail.com>
2020-08-14 13:58:26 +0530

    fix port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require ('express');
 
 var app = express();
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use (express.static(publicPath));
 
