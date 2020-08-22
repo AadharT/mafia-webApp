59f36c10cf4c866587f6be807403887ca9d0d06e
Aadhar <tyagi.aadhar@gmail.com>
2020-08-22 16:57:32 +0530

    add style to chatting

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,33 @@
-let socket = io();
+// Make connection
+var socket = io.connect("http://localhost:4000");
 
-socket.on("connect", () => {
-  console.log("Connected to server");
+// Query DOM
+var message = document.getElementById("message"),
+  handle = document.getElementById("handle"),
+  btn = document.getElementById("send"),
+  output = document.getElementById("output"),
+  feedback = document.getElementById("feedback");
 
-  socket.emit("createMessage", {
-      from : "Aadhar",
-      message : "hello"
+// Emit events
+btn.addEventListener("click", function () {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value,
   });
+  message.value = "";
 });
 
-socket.on("disconnect", () => {
-  console.log("Disconnected from server");
+message.addEventListener("keypress", function () {
+  socket.emit("typing", handle.value);
+});
+
+// Listen for events
+socket.on("chat", function (data) {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+});
+
+socket.on("typing", function (data) {
+  feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
 });
